6f9428f7d9d0c95a274689d8be256a64859486cf
Vicente Neto <sneto.vicente@gmail.com>
2017-05-26 22:54:23 -0300

    Criando testes para os serviços de ligas, time_logado, pontuacao_atleta e outros

--- cartolafc/api.py
+++ cartolafc/api.py
@@ -7,7 +7,8 @@ import requests
 
 from cartolafc.decorators import RequiresAuthentication
 from cartolafc.error import CartolaFCError, CartolaFCOverloadError
-from cartolafc.models import Atleta, Clube, DestaqueRodada, Liga, Mercado, Patrocinador, PontuacaoInfo, Time, TimeInfo
+from cartolafc.models import Atleta, Clube, DestaqueRodada, Liga, LigaPatrocinador, Mercado, PontuacaoInfo, Time
+from cartolafc.models import TimeInfo
 from cartolafc.util import convert_team_name_to_slug, parse_and_check_cartolafc
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -34,8 +35,8 @@ class Api(object):
             >>> api = cartolafc.Api()
         
         Para obter o status atual do mercado
-            >>> status = api.status()
-            >>> print(status.rodada_atual, status.status_mercado.nome)
+            >>> mercado = api.mercado()
+            >>> print(mercado.rodada_atual, mercado.status.nome)
         
         Para utilizar autenticação, é necessário instancias a classe cartolafc.Api com os argumentos email e senha.
             >>> api =  cartolafc.Api(email='usuario@email.com', password='s3nha')
@@ -45,7 +46,7 @@ class Api(object):
             >>> print(liga.nome)
 
         python-cartolafc é massa!!! E possui muitos outros métodos, como:
-            >>> api.status()
+            >>> api.mercado()
             >>> api.time(nome, slug)
             >>> api.busca_times(termo)
     """
@@ -128,21 +129,21 @@ class Api(object):
     def clubes(self):
         url = '{api_url}/clubes'.format(api_url=self._api_url)
         data = self._request(url)
-        return {clube_id: Clube.from_dict(clube) for clube_id, clube in data.items()}
+        return {int(clube_id): Clube.from_dict(clube) for clube_id, clube in data.items()}
 
     def ligas(self, query):
         url = '{api_url}/ligas'.format(api_url=self._api_url)
         data = self._request(url, params=dict(q=query))
         return [Liga.from_dict(liga_info) for liga_info in data]
 
-    def mercado(self):
-        url = '{api_url}/atletas/mercado'.format(api_url=self._api_url)
+    def ligas_patrocinadores(self):
+        url = '{api_url}/patrocinadores'.format(api_url=self._api_url)
         data = self._request(url)
-        clubes = {clube['id']: Clube.from_dict(clube) for clube in data['clubes'].values()}
-        return [Atleta.from_dict(atleta, clubes=clubes) for atleta in data['atletas']]
+        return {int(patrocinador_id): LigaPatrocinador.from_dict(patrocinador) for patrocinador_id, patrocinador in
+                data.items()}
 
-    def status(self):
-        """ Obtém o status do mercado na rodada atual. 
+    def mercado(self):
+        """ Obtém o status do mercado na rodada atual.
 
         Returns:
             Uma instância de cartolafc.Mercado representando o status do mercado na rodada atual.
@@ -152,23 +153,24 @@ class Api(object):
         data = self._request(url)
         return Mercado.from_dict(data)
 
+    def mercado_atletas(self):
+        url = '{api_url}/atletas/mercado'.format(api_url=self._api_url)
+        data = self._request(url)
+        clubes = {clube['id']: Clube.from_dict(clube) for clube in data['clubes'].values()}
+        return [Atleta.from_dict(atleta, clubes=clubes) for atleta in data['atletas']]
+
     def parciais(self):
-        if self.status().status_mercado.id == MERCADO_FECHADO:
+        if self.mercado().status.id == MERCADO_FECHADO:
             url = '{api_url}/atletas/pontuados'.format(api_url=self._api_url)
             data = self._request(url)
             clubes = {clube['id']: Clube.from_dict(clube) for clube in data['clubes'].values()}
-            return {atleta_id: Atleta.from_dict(atleta, clubes=clubes, atleta_id=atleta_id) for atleta_id, atleta
+            return {int(atleta_id): Atleta.from_dict(atleta, clubes=clubes, atleta_id=atleta_id) for atleta_id, atleta
                     in data['atletas'].items()}
 
         raise CartolaFCError('As pontuações parciais só ficam disponíveis com o mercado fechado.')
 
-    def patrocinadores(self):
-        url = '{api_url}/patrocinadores'.format(api_url=self._api_url)
-        data = self._request(url)
-        return {patrocinador_id: Patrocinador.from_dict(patrocinador) for patrocinador_id, patrocinador in data.items()}
-
     def pos_rodada_destaques(self):
-        if self.status().status_mercado.id == MERCADO_ABERTO:
+        if self.mercado().status.id == MERCADO_ABERTO:
             url = '{api_url}/pos-rodada/destaques'.format(api_url=self._api_url)
             data = self._request(url)
             return DestaqueRodada.from_dict(data)
--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,8 @@ import requests_mock
 
 import cartolafc
 from cartolafc.api import MERCADO_ABERTO
-from cartolafc.models import Mercado, TimeInfo
+from cartolafc.models import Atleta, Clube, Liga, LigaPatrocinador, Mercado, PontuacaoInfo, Time, TimeInfo
+from cartolafc.models import _atleta_status, _posicoes
 
 
 class ApiAttemptsTest(unittest.TestCase):
@@ -35,7 +36,7 @@ class ApiAttemptsTest(unittest.TestCase):
             m.get(url, status_code=200, text='{"mensagem": "%s"}' % error_message)
 
             with self.assertRaisesRegexp(cartolafc.CartolaFCError, error_message):
-                api.status()
+                api.mercado()
 
 
 class ApiAuthComErro(unittest.TestCase):
@@ -85,12 +86,16 @@ class ApiAuthComErro(unittest.TestCase):
 
             # Act and Assert
             with self.assertRaises(cartolafc.CartolaFCOverloadError):
-                api.status()
+                api.mercado()
 
 
 class ApiAuthTest(unittest.TestCase):
     with open('testdata/amigos.json', 'rb') as f:
         AMIGOS = f.read().decode('utf8')
+    with open('testdata/pontuacao_atleta.json', 'rb') as f:
+        PONTUACAO_ATLETA = f.read().decode('utf8')
+    with open('testdata/time_logado.json', 'rb') as f:
+        TIME_LOGADO = f.read().decode('utf8')
 
     def setUp(self):
         with requests_mock.mock() as m:
@@ -111,16 +116,74 @@ class ApiAuthTest(unittest.TestCase):
             # Assert
             self.assertIsInstance(amigos, list)
             self.assertIsInstance(primeiro_time, TimeInfo)
-            self.assertEqual(primeiro_time.time_id, 22463)
+            self.assertEqual(primeiro_time.id, 22463)
             self.assertEqual(primeiro_time.nome, u'UNIÃO BRUNÃO F.C')
             self.assertEqual(primeiro_time.nome_cartola, 'Bruno Nascimento')
             self.assertEqual(primeiro_time.slug, 'uniao-brunao-f-c')
             self.assertFalse(primeiro_time.assinante)
 
+    def test_pontuacao_atleta(self):
+        # Arrange and Act
+        with requests_mock.mock() as m:
+            url = '{api_url}/auth/mercado/atleta/{id}/pontuacao'.format(api_url=self.api_url, id=81682)
+            m.get(url, text=self.PONTUACAO_ATLETA)
+            pontuacoes = self.api.pontuacao_atleta(81682)
+            primeira_rodada = pontuacoes[0]
+
+            # Assert
+            self.assertIsInstance(pontuacoes, list)
+            self.assertIsInstance(primeira_rodada, PontuacaoInfo)
+            self.assertEqual(primeira_rodada.atleta_id, 81682)
+            self.assertEqual(primeira_rodada.rodada_id, 1)
+            self.assertEqual(primeira_rodada.pontos, 1.1)
+            self.assertEqual(primeira_rodada.preco, 6.44)
+            self.assertEqual(primeira_rodada.variacao, -1.56)
+            self.assertEqual(primeira_rodada.media, 1.1)
+
+    def test_time_logado(self):
+        # Arrange and Act
+        with requests_mock.mock() as m:
+            url = '{api_url}/auth/time'.format(api_url=self.api_url)
+            m.get(url, text=self.TIME_LOGADO)
+            time = self.api.time_logado()
+            primeiro_atleta = time.atletas[0]
+
+            # Assert
+            self.assertIsInstance(time, Time)
+            self.assertEqual(time.patrimonio, 144.74)
+            self.assertEqual(time.valor_time, 143.84961)
+            self.assertEqual(time.ultima_pontuacao, 70.02978515625)
+            self.assertIsInstance(time.atletas, list)
+            self.assertIsInstance(primeiro_atleta, Atleta)
+            self.assertEqual(primeiro_atleta.id, 38140)
+            self.assertEqual(primeiro_atleta.apelido, 'Fernando Prass')
+            self.assertEqual(primeiro_atleta.pontos, 7.5)
+            self.assertEqual(primeiro_atleta.scout, {'DD': 5, 'FS': 1, 'GS': 1, 'PE': 1, 'SG': 1})
+            self.assertEqual(primeiro_atleta.posicao, _posicoes[1])
+            self.assertIsInstance(primeiro_atleta.clube, Clube)
+            self.assertEqual(primeiro_atleta.clube.id, 275)
+            self.assertEqual(primeiro_atleta.clube.nome, 'Palmeiras')
+            self.assertEqual(primeiro_atleta.clube.abreviacao, 'PAL')
+            self.assertEqual(primeiro_atleta.status, _atleta_status[7])
+            self.assertIsInstance(time.info, TimeInfo)
+            self.assertEqual(time.info.id, 471815)
+            self.assertEqual(time.info.nome, 'Falydos FC')
+            self.assertEqual(time.info.nome_cartola, 'Vicente Neto')
+            self.assertEqual(time.info.slug, 'falydos-fc')
+            self.assertTrue(time.info.assinante)
+
 
 class ApiTest(unittest.TestCase):
-    with open('testdata/status_mercado_aberto.json', 'rb') as f:
-        STATUS_MERCADO_ABERTO = f.read().decode('utf8')
+    with open('testdata/clubes.json', 'rb') as f:
+        CLUBES = f.read().decode('utf8')
+    with open('testdata/ligas.json', 'rb') as f:
+        LIGAS = f.read().decode('utf8')
+    with open('testdata/ligas_patrocinadores.json', 'rb') as f:
+        LIGAS_PATROCINADORES = f.read().decode('utf8')
+    with open('testdata/mercado_atletas.json', 'rb') as f:
+        MERCADO_ATLETAS = f.read().decode('utf8')
+    with open('testdata/mercado_status_aberto.json', 'rb') as f:
+        MERCADO_STATUS_ABERTO = f.read().decode('utf8')
     with open('testdata/times.json', 'rb') as f:
         TIMES = f.read().decode('utf-8')
 
@@ -133,21 +196,104 @@ class ApiTest(unittest.TestCase):
         with self.assertRaisesRegexp(cartolafc.CartolaFCError, 'Esta função requer autenticação'):
             self.api.amigos()
 
-    def test_status(self):
+    def test_clubes(self):
+        # Arrange and Act
+        with requests_mock.mock() as m:
+            url = '{api_url}/clubes'.format(api_url=self.api_url)
+            m.get(url, text=self.CLUBES)
+            clubes = self.api.clubes()
+            clube_flamengo = clubes[262]
+
+            # Assert
+            self.assertIsInstance(clubes, dict)
+            self.assertIsInstance(clube_flamengo, Clube)
+            self.assertEqual(clube_flamengo.id, 262)
+            self.assertEqual(clube_flamengo.nome, 'Flamengo')
+            self.assertEqual(clube_flamengo.abreviacao, 'FLA')
+
+    def test_ligas(self):
+        # Arrange and Act
+        with requests_mock.mock() as m:
+            url = '{api_url}/ligas'.format(api_url=self.api_url)
+            m.get(url, text=self.LIGAS)
+            ligas = self.api.ligas(query='premiere')
+            primeira_liga = ligas[0]
+
+            # Assert
+            self.assertIsInstance(ligas, list)
+            self.assertIsInstance(primeira_liga, Liga)
+            self.assertEqual(primeira_liga.id, 36741)
+            self.assertEqual(primeira_liga.nome, 'PREMIERE_LIGA_ENTEL')
+            self.assertEqual(primeira_liga.slug, 'premiere-liga-entel')
+            self.assertEqual(primeira_liga.descricao, u'“Vale tudo, só não vale...”')
+            self.assertIsNone(primeira_liga.times)
+
+    def test_mercado(self):
         # Arrange and Act
         with requests_mock.mock() as m:
             url = '{api_url}/mercado/status'.format(api_url=self.api_url)
-            m.get(url, text=self.STATUS_MERCADO_ABERTO)
-            status = self.api.status()
+            m.get(url, text=self.MERCADO_STATUS_ABERTO)
+            status = self.api.mercado()
 
             # Assert
             self.assertIsInstance(status, Mercado)
             self.assertEqual(status.rodada_atual, 3)
-            self.assertEqual(status.status_mercado.id, MERCADO_ABERTO)
+            self.assertEqual(status.status.id, MERCADO_ABERTO)
             self.assertEqual(status.times_escalados, 3601523)
             self.assertIsInstance(status.fechamento, datetime)
             self.assertEqual(status.fechamento, datetime.fromtimestamp(1495904400))
             self.assertEqual(status.aviso, '')
+
+    def test_mercado_atletas(self):
+        # Arrange and Act
+        with requests_mock.mock() as m:
+            url = '{api_url}/atletas/mercado'.format(api_url=self.api_url)
+            m.get(url, text=self.MERCADO_ATLETAS)
+            mercado = self.api.mercado_atletas()
+            primeiro_atleta = mercado[0]
+
+            # Assert
+            self.assertIsInstance(mercado, list)
+            self.assertIsInstance(primeiro_atleta, Atleta)
+            self.assertEqual(primeiro_atleta.id, 86935)
+            self.assertEqual(primeiro_atleta.apelido, 'Rodrigo')
+            self.assertEqual(primeiro_atleta.pontos, 0)
+            self.assertEqual(primeiro_atleta.scout, {'CA': 1, 'FC': 3, 'FS': 1, 'PE': 2, 'RB': 2})
+            self.assertEqual(primeiro_atleta.posicao, _posicoes[4])
+            self.assertIsInstance(primeiro_atleta.clube, Clube)
+            self.assertEqual(primeiro_atleta.clube.id, 292)
+            self.assertEqual(primeiro_atleta.clube.nome, 'Sport')
+            self.assertEqual(primeiro_atleta.clube.abreviacao, 'SPO')
+            self.assertEqual(primeiro_atleta.status, _atleta_status[6])
+
+    def test_parciais_mercado_fechado(self):
+        # Arrange
+        with requests_mock.mock() as m:
+            url = '{api_url}/mercado/status'.format(api_url=self.api_url)
+            m.get(url, text=self.MERCADO_STATUS_ABERTO)
+
+            # Act and Assert
+            with self.assertRaisesRegexp(cartolafc.CartolaFCError,
+                                         'As pontuações parciais só ficam disponíveis com o mercado fechado.'):
+                self.api.parciais()
+
+    def test_patrocinadores(self):
+        # Arrange and Act
+        with requests_mock.mock() as m:
+            url = '{api_url}/patrocinadores'.format(api_url=self.api_url)
+            m.get(url, text=self.LIGAS_PATROCINADORES)
+            ligas = self.api.ligas_patrocinadores()
+            liga_brahma = ligas[62]
+
+            # Assert
+            self.assertIsInstance(ligas, dict)
+            self.assertIsInstance(liga_brahma, LigaPatrocinador)
+            self.assertEqual(liga_brahma.id, 62)
+            self.assertEqual(liga_brahma.nome, 'Cerveja Brahma')
+            self.assertEqual(liga_brahma.url_link, 'http://brahma.com.br')
+
+    def test_pos_rodada_destaques(self):
+        pass
 
     def test_times(self):
         # Arrange and Act
@@ -160,7 +306,7 @@ class ApiTest(unittest.TestCase):
             # Assert
             self.assertIsInstance(times, list)
             self.assertIsInstance(primeiro_time, TimeInfo)
-            self.assertEqual(primeiro_time.time_id, 4626963)
+            self.assertEqual(primeiro_time.id, 4626963)
             self.assertEqual(primeiro_time.nome, 'Falysson29')
             self.assertEqual(primeiro_time.nome_cartola, 'Alysson')
             self.assertEqual(primeiro_time.slug, 'falysson29')
@@ -174,4 +320,4 @@ class ApiTest(unittest.TestCase):
 
             # Act and Assert
             with self.assertRaises(cartolafc.CartolaFCOverloadError):
-                self.api.status()
+                self.api.mercado()
